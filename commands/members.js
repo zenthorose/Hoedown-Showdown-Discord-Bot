cc4587ff1b68fddabcdef92f2c7f41d512ba6c3d
zenthorose <Zenthorose@gmail.com>
2025-03-13 21:28:11 -0400

    Update members.js
    Updating to Grab User ID numbers as well as sending names in a txt file.

--- commands/members.js
+++ commands/members.js
@@ -3,14 +3,21 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('members')
-        .setDescription('Fetches a list of all server members.'),
+        .setDescription('Fetches a list of all server members with their IDs.'),
     async execute(interaction) {
         await interaction.guild.members.fetch(); // Ensures cache is populated
-        const members = interaction.guild.members.cache.map(member => `${member.user.username}#${member.user.discriminator}`).join('\n');
-        
-        if (members.length > 2000) {
-            return interaction.reply('Member list is too long to display.');
-        }
+        const members = interaction.guild.members.cache
+            .map(member => `${member.user.username}#${member.user.discriminator} (ID: ${member.user.id})`)
+            .join('\n');
+
+            const { MessageAttachment } = require('discord.js');
+            const fs = require('fs');
+            
+            if (members.length > 2000) {
+                fs.writeFileSync('members.txt', members);
+                const attachment = new MessageAttachment('members.txt');
+                return interaction.reply({ content: 'Member list:', files: [attachment] });
+            }
 
         await interaction.reply(`**Server Members:**\n${members}`);
     },
