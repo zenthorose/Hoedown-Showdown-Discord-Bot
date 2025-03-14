3590766d563349295f76455bdc8e8be84f43ba33
zenthorose <Zenthorose@gmail.com>
2025-03-14 00:05:10 -0400

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
-const { MaleEmoji, MaleName, Hoedown_New_banner, FemaleName } = require('../config.json');
+const { MaleEmoji, MaleName, FemaleEmoji, FemaleName } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
