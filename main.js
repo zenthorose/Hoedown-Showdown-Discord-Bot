2c140645069b1003f17d6d54e9c8e17ccf41127c
zenthorose <Zenthorose@gmail.com>
2025-03-15 18:25:55 -0400

    Updated Google Credentials

--- main.js
+++ main.js
@@ -28,7 +28,19 @@ const reactionPostsManager = new ReactionPostsManager();
 const { Hoedown_New_banner, statusChannelId } = require('./config.json');
 
 const SPREADSHEET_ID = "YOUR_SPREADSHEET_ID_HERE";
-const credentials = require('./service-account.json'); // Your Google API Key JSON file
+const credentials = {
+    type: "service_account",
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fixes key formatting
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT
+};
+
 
 // Load commands from /commands/ folder
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
