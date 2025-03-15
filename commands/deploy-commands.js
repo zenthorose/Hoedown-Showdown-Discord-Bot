9982572dbab27ae5402630247ef4577fb2678589
zenthorose <Zenthorose@gmail.com>
2025-03-15 14:05:50 -0400

    Added Deploy Commands

---
+++ commands/deploy-commands.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+(async () => {
+    try {
+        console.log('🔄 Refreshing application (/) commands...');
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+        console.log('✅ Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error('❌ Failed to refresh commands:', error);
+    }
+})();
