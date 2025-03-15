a50166009e43fd9cf05ed2be2df8abb60be517da
zenthorose <Zenthorose@gmail.com>
2025-03-15 18:30:36 -0400

    Update members.js

--- commands/members.js
+++ commands/members.js
@@ -1,7 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { google } = require('googleapis');
 const fs = require('fs');
-const credentials = require('../service-account.json'); // Your JSON key file
+const credentials = {
+    type: "service_account",
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fixes key formatting
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT
+};
+
 
 const SPREADSHEET_ID = "YOUR_SPREADSHEET_ID_HERE";
 
