692cf1e8215feb9f10d6d4e040b9ad8e189bf73e
zenthorose <Zenthorose@gmail.com>
2025-03-15 15:58:30 -0400

    Revert "Removed need for Channel ID"
    This reverts commit 7ef1fa3dfb45647f98c3526bd26f43e4157953a1.

--- commands/reactions.js
+++ commands/reactions.js
@@ -4,36 +4,26 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('reactions')
-        .setDescription('Fetches reactions from a specific message without needing a channel ID.')
+        .setDescription('Fetches reactions from a specific message in any channel.')
         .addStringOption(option =>
             option.setName('messageid')
                 .setDescription('The ID of the message to check reactions for')
                 .setRequired(true)
+        )
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('The channel where the message is located')
+                .setRequired(true)
         ),
-
+    
     async execute(interaction) {
         const messageId = interaction.options.getString('messageid');
+        const targetChannel = interaction.options.getChannel('channel'); // Get the specified channel
 
         try {
-            let targetMessage = null;
-
-            // Search all accessible channels for the message
-            for (const [channelId, channel] of interaction.guild.channels.cache) {
-                if (channel.isTextBased()) {
-                    try {
-                        targetMessage = await channel.messages.fetch(messageId);
-                        if (targetMessage) break; // Stop searching once found
-                    } catch (err) {
-                        continue; // Ignore errors and move to the next channel
-                    }
-                }
-            }
-
-            if (!targetMessage) {
-                return await interaction.reply({ content: `Message with ID ${messageId} not found in any accessible channel.`, ephemeral: true });
-            }
-
+            const targetMessage = await targetChannel.messages.fetch(messageId);
             const reactions = targetMessage.reactions.cache;
+
             if (reactions.size > 0) {
                 const embed = new EmbedBuilder()
                     .setColor('#444444')
@@ -56,7 +46,7 @@ module.exports = {
             }
         } catch (error) {
             console.error(error);
-            await interaction.reply({ content: `Failed to fetch reactions for message ID ${messageId}.`, ephemeral: true });
+            await interaction.reply(`Failed to fetch reactions for message ID ${messageId}.`);
         }
     },
 };
