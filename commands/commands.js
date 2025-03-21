80d6580b37dda2de52d44ba763af4e1cae718e01
zenthorose <Zenthorose@gmail.com>
2025-03-21 18:29:20 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -18,9 +18,15 @@ client.once('ready', () => {
 
 // Define commands
 const commands = [
+    { name: '/info', description: 'Provides bot information' },
     { name: '/commands', description: 'Shows this list of commands' },
-    { name: '/ping', description: 'Checks bot latency' },
-    { name: '/info', description: 'Provides bot information' }
+    { name: '/members', description: 'Gathers a list of all members of the discord before sending it to the google sheet. This command is ran automatically each time the bot is launched.' },
+    { name: '/purgebot', description: 'Deletes all previous messages from the bot in whatever channel the command was entered in.' },
+    { name: '/reactions', description: 'Sends the list of reactions from the selected message to the google sheet to begin the team creation.' },
+    { name: '/reactionrole', description: 'Posts all of the timeslot posts for people to opt in or out of.' },
+    { name: '/testreactions', description: 'Temp command will be removed and likely wont work currently.' },
+    { name: '/ping', description: 'Checks bot latency' }
+    
 ];
 
 client.on('interactionCreate', async (interaction) => {
@@ -41,7 +47,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
         console.log('Refreshing application (/) commands...');
 
         const commandData = [
-            new SlashCommandBuilder().setName('help').setDescription('Shows a list of available commands')
+            new SlashCommandBuilder().setName('commands').setDescription('Shows a list of available commands')
         ].map(cmd => cmd.toJSON());
 
         if (GUILD_ID) {
