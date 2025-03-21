8e67e0b686a922d60cb513d7fe07421a4563baa9
zenthorose <Zenthorose@gmail.com>
2025-03-21 18:36:05 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -16,39 +16,36 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-// Define commands
+// Define all commands
 const commands = [
-    { name: '/info', description: 'Provides bot information' },
-    { name: '/commands', description: 'Shows this list of commands' },
-    { name: '/members', description: 'Gathers a list of all members of the discord before sending it to the google sheet. This command is ran automatically each time the bot is launched.' },
-    { name: '/purgebot', description: 'Deletes all previous messages from the bot in whatever channel the command was entered in.' },
-    { name: '/reactions', description: 'Sends the list of reactions from the selected message to the google sheet to begin the team creation.' },
-    { name: '/reactionrole', description: 'Posts all of the timeslot posts for people to opt in or out of.' },
-    { name: '/testreactions', description: 'Temp command will be removed and likely wont work currently.' },
-    { name: '/ping', description: 'Checks bot latency' }
-    
+    { name: 'info', description: 'Provides bot information' },
+    { name: 'commands', description: 'Shows this list of commands' },
+    { name: 'members', description: 'Gathers a list of all members of the discord before sending it to the google sheet. This command is ran automatically each time the bot is launched.' },
+    { name: 'purgebot', description: 'Deletes all previous messages from the bot in whatever channel the command was entered in.' },
+    { name: 'reactions', description: 'Sends the list of reactions from the selected message to the google sheet to begin the team creation.' },
+    { name: 'reactionrole', description: 'Posts all of the timeslot posts for people to opt in or out of.' },
+    { name: 'testreactions', description: 'Temp command will be removed and likely wont work currently.' },
+    { name: 'ping', description: 'Checks bot latency' }
 ];
 
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === 'commands') {
-        let commandList = commands.map(cmd => `**${cmd.name}** - ${cmd.description}`).join('\n');
+        let commandList = commands.map(cmd => `**/${cmd.name}** - ${cmd.description}`).join('\n');
 
         await interaction.reply({ content: `Here are my commands:\n\n${commandList}`, ephemeral: true });
     }
 });
 
-// Register commands
+// Register all commands
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 (async () => {
     try {
         console.log('Refreshing application (/) commands...');
 
-        const commandData = [
-            new SlashCommandBuilder().setName('commands').setDescription('Shows a list of available commands')
-        ].map(cmd => cmd.toJSON());
+        const commandData = commands.map(cmd => new SlashCommandBuilder().setName(cmd.name).setDescription(cmd.description)).map(cmd => cmd.toJSON());
 
         if (GUILD_ID) {
             await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commandData });
