970fc111003e488718977b1fae959e238acadc89
zenthorose <Zenthorose@gmail.com>
2025-03-26 15:19:38 -0400

    Final Phase work.
    Removed test reactions.
    Updated Command List.
    Added in the blank approve teams script.

--- commands/commands-info.js
+++ commands/commands-info.js
@@ -28,13 +28,14 @@ module.exports = {
         }
 
         const commands = [
-            { name: '/info', description: 'Provides bot information' },
+            { name: '/commands-info', description: 'Provides bot information' },
             { name: '/member-update', description: 'Gathers a list of all members of the discord before sending it to the Google Sheets.' },
             { name: '/message-purge', description: 'Deletes all previous messages from the bot in whatever channel the command was entered in.' },
-            { name: '/grab-reactions', description: 'Sends the list of reactions from the selected message to the Google Sheets to begin the team creation.' },
             { name: '/time-slots', description: 'Posts all of the timeslot posts for people to opt in or out of.' },
-            { name: '/testreactions', description: 'Temp command, will be removed and likely won’t work currently.' },
-            { name: '/ping', description: 'Checks bot latency' }
+            { name: '/grab-reactions', description: 'Sends the list of reactions from the selected message to the Google Sheets to begin the team creation.' },
+            { name: '/swap', description: 'Used to replace members on teams before official list is posted. Enter Team Set # followed by the person you are replacing lastly who you are replacing them with.' },
+            { name: '/approve-teams', description: 'Use this command after the team has been reviewed and everything is good to publish the official Team List to everyone.' },
+            { name: '/ping', description: 'This keeps the bot alive.' }
         ];
 
         let commandList = commands.map(cmd => `\"${cmd.name}\" - ${cmd.description}`).join('\n');
