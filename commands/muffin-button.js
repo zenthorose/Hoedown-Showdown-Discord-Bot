a657d0742e3fedd3d4b9e7a59be0b6acc6a7d206
zenthorose <Zenthorose@gmail.com>
2025-03-27 22:58:55 -0400

    Update muffin-button.js

--- commands/muffin-button.js
+++ commands/muffin-button.js
@@ -1,26 +1,25 @@
-const { ButtonBuilder, ButtonStyle } = require('discord.js'); // Import the ButtonBuilder class
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js'); // Updated imports
 
 module.exports = {
-    data: {
-        name: 'muffin-button',
-        description: 'Send a muffin button',
-    },
+    data: new SlashCommandBuilder()
+        .setName('muffin-button')
+        .setDescription('Sends a muffin button for users to click!'),
+    
     async execute(interaction) {
         // Create the muffin button using ButtonBuilder
-        const button = new ButtonBuilder()
-            .setCustomId('muffin')
-            .setLabel('Click me!')
-            .setStyle(ButtonStyle.Primary); // You can change the style here
+        const muffinButton = new ButtonBuilder()
+            .setCustomId('muffin') // The custom ID used to identify the button click
+            .setLabel('Muffin Button') // Button label
+            .setStyle(ButtonStyle.Primary); // Button style (Primary, Secondary, Danger, Link)
+
+        // Create a row to hold the button using ActionRowBuilder
+        const row = new ActionRowBuilder().addComponents(muffinButton);
 
         // Send the message with the button
         await interaction.reply({
-            content: 'Here is your muffin button!',
-            components: [
-                {
-                    type: 1, // Action row type
-                    components: [button], // Attach the button to the action row
-                },
-            ],
+            content: 'Muffin!',
+            components: [row],
         });
     },
 };
