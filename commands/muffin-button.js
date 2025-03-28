fd00f51d2bb3d91bf8104a29d022a46bd817186e
zenthorose <Zenthorose@gmail.com>
2025-03-27 23:09:53 -0400

    Update muffin-button.js

--- commands/muffin-button.js
+++ commands/muffin-button.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js'); // Updated imports
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const config = require('./config.json'); // Assuming the roles and user IDs are in a config file
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,6 +8,27 @@ module.exports = {
         .setDescription('Sends a muffin button for users to click!'),
     
     async execute(interaction) {
+        // Fetch the allowed roles and user IDs from the config file
+        const allowedRoles = config.allowedRoles;
+        const allowedUserIds = config.allowedUserIds;
+
+        // Check if the user has the required role or the specific Discord ID
+        const member = await interaction.guild.members.fetch(interaction.user.id);
+        
+        // Check if the user has any of the allowed roles
+        const hasRequiredRole = member.roles.cache.some(role => allowedRoles.includes(role.name));
+        
+        // Check if the user's Discord ID is in the allowed list
+        const isAllowedUser = allowedUserIds.includes(interaction.user.id);
+
+        // If the user doesn't have the required role or ID, deny access
+        if (!hasRequiredRole && !isAllowedUser) {
+            return interaction.reply({
+                content: '❌ You do not have the required permissions to use this command.',
+                ephemeral: true, // Sends the reply privately to the user
+            });
+        }
+
         // Create the muffin button using ButtonBuilder
         const muffinButton = new ButtonBuilder()
             .setCustomId('muffin') // The custom ID used to identify the button click
