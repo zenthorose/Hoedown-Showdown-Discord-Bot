89eb2b4267bc635e1ef56266998e52363a7ef951
zenthorose <Zenthorose@gmail.com>
2026-04-15 23:59:01 -0400

    hoedown-reset

--- commands/commands.js
+++ commands/commands.js
@@ -78,6 +78,7 @@ module.exports = {
         { name: '/replace', description: 'You must enter the Round #, the person to remove from the list and then person to add that isn’t on the list.' },
         { name: '/swap', description: 'Swap’s the postion of 2-10 people on the list.' },
         { name: '/approve-round', description: 'Publishes the final Team List to the correct round channel and @’s everyone for that round.' },
+        { name: '/hoedown-reset', description: 'Resets all hoedown essential channels to a fresh state.' },
         { name: '/avoid', description: 'Let’s you add up to 5 people to the avoid list.' },
         { name: '/unavoid', description: 'Let’s you remove an avoided pair from the bot.' },
         { name: '/avoid-list', description: 'This allows you to pull the entire avoid list or just the list of a user if you add the name.' },
